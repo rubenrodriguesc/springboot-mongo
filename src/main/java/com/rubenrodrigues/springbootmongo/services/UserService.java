43d709226e16e99ed0b95d63f7e723e0a9396b10
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-02-14 21:44:33 -0300

    Obtendo um usuário por id

--- src/main/java/com/rubenrodrigues/springbootmongo/services/UserService.java
+++ src/main/java/com/rubenrodrigues/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rubenrodrigues.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rubenrodrigues.springbootmongo.domain.User;
 import com.rubenrodrigues.springbootmongo.repositories.UserRepository;
+import com.rubenrodrigues.springbootmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
