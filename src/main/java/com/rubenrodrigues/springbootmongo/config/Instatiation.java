08dc3ba019c34baa75164ac70112b35c18a8fb52
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-02-23 12:07:37 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/rubenrodrigues/springbootmongo/config/Instatiation.java
+++ src/main/java/com/rubenrodrigues/springbootmongo/config/Instatiation.java
@@ -9,34 +9,38 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rubenrodrigues.springbootmongo.domain.Post;
 import com.rubenrodrigues.springbootmongo.domain.User;
+import com.rubenrodrigues.springbootmongo.dto.AuthorDTO;
 import com.rubenrodrigues.springbootmongo.repositories.PostRepository;
 import com.rubenrodrigues.springbootmongo.repositories.UserRepository;
 
 @Configuration
 public class Instatiation implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail");
 		User alex = new User(null, "Alex Green", "alex@gmail");
 		User bob = new User(null, "Bob Grey", "bob@gmail");
-		
-		Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00.0Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00.0Z"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));	
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00.0Z"), "Partiu viagem",
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00.0Z"), "Bom dia", "Acordei feliz hoje!",
+				new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 	}
 
 }
--- src/main/java/com/rubenrodrigues/springbootmongo/domain/Post.java
+++ src/main/java/com/rubenrodrigues/springbootmongo/domain/Post.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rubenrodrigues.springbootmongo.dto.AuthorDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,13 +20,13 @@ public class Post implements Serializable {
 	private Instant instant;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 
 	}
 
-	public Post(String id, Instant instant, String title, String body, User author) {
+	public Post(String id, Instant instant, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.instant = instant;
@@ -66,11 +67,11 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
