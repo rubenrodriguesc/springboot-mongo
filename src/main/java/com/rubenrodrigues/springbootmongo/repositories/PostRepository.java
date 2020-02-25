9690a22ceeb7c7fbbe267ad0313d211cffad6827
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-02-25 00:51:12 -0300

    Consulta simples com @Query

--- src/main/java/com/rubenrodrigues/springbootmongo/repositories/PostRepository.java
+++ src/main/java/com/rubenrodrigues/springbootmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.rubenrodrigues.springbootmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rubenrodrigues.springbootmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.rubenrodrigues.springbootmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findTitle(String text);
 
 }
